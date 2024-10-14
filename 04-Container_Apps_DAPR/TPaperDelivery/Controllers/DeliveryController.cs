5aaa0408b8e9e7b2f1241a7927b92a44b9f39ef3
Stas Lebedenko <stas.lebedenko@solidify.dev>
2024-10-14 20:37:35 +0200

    Container app with DAPR

---
+++ 04-Container_Apps_DAPR/TPaperDelivery/Controllers/DeliveryController.cs
@@ -0,0 +1,51 @@
+using Dapr;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPaperDelivery
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DeliveryController
+    {
+        private readonly ILogger<DeliveryController> _logger;
+
+        public DeliveryController(ILogger<DeliveryController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Topic("pubsubsbus", "createdelivery")]
+        [HttpPost]
+        [Route("createdelivery")]
+        public async Task<IActionResult> ProcessEdiOrder(Delivery delivery)
+        {
+            _logger.LogWarning("Triggered method");
+
+            var newDelivery = new Delivery
+            {
+                Id = 0,
+                ClientId = delivery.ClientId,
+                EdiOrderId = delivery.EdiOrderId,
+                Number = delivery.Number,
+                ProductId = 1,
+                ProductCode = 333,
+                Notes = "Prepared for shipment"
+            };
+
+            _logger.LogWarning("Saved delivery");
+
+            return new OkObjectResult("");
+        }
+        
+        [HttpGet]
+        [Route("health")]
+        public async Task<IActionResult> Health(CancellationToken cts)
+        {
+            return new OkObjectResult("Started");
+        }
+    }
+}
